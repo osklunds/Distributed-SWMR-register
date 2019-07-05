3c9d265fcafed6519b81f8146e0a5d3732c27d42
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-07-05 22:27:54 +0200

    Add basic command line use in local_starter.

--- local_starter/src/main.rs
+++ local_starter/src/main.rs
@@ -1,3 +1,18 @@
+
+use std::process::Command;
+use std::str;
+use std::{thread, time};
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut child = Command::new("/bin/bash")
+                .env("RED", "\\033[0;31m")
+                .arg("-c")
+                .arg("printf \"I ${RED}love Stack Overflow\n\"")
+                .spawn()
+                .expect("failed to execute process");
+
+
+    thread::sleep(time::Duration::from_millis(5000));
+    child.kill();
 }
