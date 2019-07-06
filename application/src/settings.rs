f18925fffec82613b4d3ef0ef02d0d107a5619db
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-07-06 08:35:12 +0200

    Organize the arguments retrieval.

--- application/src/settings.rs
+++ application/src/settings.rs
@@ -5,7 +5,7 @@ use std::net::Ipv4Addr;
 use std::fs;
 
 use colored::*;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App, ArgMatches};
 
 use crate::register::NodeId;
 
@@ -25,39 +25,10 @@ pub struct Settings {
 
 impl Settings {
     fn new() -> Settings {
-        let colors = &["Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White"];
-        let matches = App::new("Distributed SWMR registers")
-                          .version("0.1")
-                          .author("Oskar Lundström")
-                          .about("Todo")
-                          .arg(Arg::with_name("node-id")
-                                .required(true)
-                                .help("The integer id of this node instance."))
-                          .arg(Arg::with_name("hosts-file")
-                                .required(true)
-                                .help("The file with host ids, addresses and ports."))
-                          .arg(Arg::with_name("color")
-                                .takes_value(true)
-                                .possible_values(colors)
-                                .help("Sets the color of the terminal output"))
-                        .get_matches();
-
-        let color;
-
-        if matches.is_present("color") {
-            color = Color::from(matches.value_of("color").unwrap());
-        } else {
-            color = Color::Black;
-        }     
-
-        let hosts_file_path = matches.value_of("hosts-file").unwrap();
-
-        let string = fs::read_to_string(hosts_file_path).expect("Unable to read file");
-        let socket_addrs = socket_addrs_from_string(string);
-
-        let node_id = matches.value_of("node-id").unwrap().parse().unwrap();
-
-
+        let matches = get_matches();
+        let color = color_from_matches(&matches);
+        let socket_addrs = socket_addrs_from_matches(&matches);
+        let node_id = node_id_from_matches(&matches);
 
         Settings {
             node_id: node_id,
@@ -65,6 +36,36 @@ impl Settings {
             terminal_color: color
         }
     }
+}
+
+fn get_matches() -> ArgMatches<'static> {
+    let colors = &["Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White"];
+    App::new("Distributed SWMR registers")
+        .version("0.1")
+        .author("Oskar Lundström")
+        .about("Todo")
+        .arg(Arg::with_name("node-id")
+            .required(true)
+            .help("The integer id of this node instance."))
+        .arg(Arg::with_name("hosts-file")
+            .required(true)
+            .help("The file with host ids, addresses and ports."))
+        .arg(Arg::with_name("color")
+            .takes_value(true)
+            .possible_values(colors)
+            .default_value("Black")
+            .help("Sets the color of the terminal output"))
+        .get_matches()
+}
+
+fn color_from_matches(matches: &ArgMatches<'static>) -> Color {
+    matches.value_of("color").unwrap().parse().unwrap()
+}
+
+fn socket_addrs_from_matches(matches: &ArgMatches<'static>) -> HashMap<NodeId, SocketAddr> {
+    let hosts_file_path = matches.value_of("hosts-file").unwrap();
+    let string = fs::read_to_string(hosts_file_path).expect("Unable to read file");
+    socket_addrs_from_string(string)
 }
 
 fn socket_addrs_from_string(string: String) -> HashMap<NodeId, SocketAddr> {
@@ -79,4 +80,8 @@ fn socket_addrs_from_string(string: String) -> HashMap<NodeId, SocketAddr> {
     }
 
     socket_addrs
+}
+
+fn node_id_from_matches(matches: &ArgMatches<'static>) -> NodeId {
+    matches.value_of("node-id").unwrap().parse().unwrap()
 }
