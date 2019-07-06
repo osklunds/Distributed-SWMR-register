7984e2cf1c22a6ab612170878f85f84c627a06f2
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-07-06 14:58:52 +0200

    Add tests for Entry.

--- application/src/entry.rs
+++ application/src/entry.rs
@@ -55,3 +55,54 @@ impl<V> Ord for Entry<V> {
         self.ts.cmp(&other.ts)
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display() {
+        let correct = "[ts = 2, val = Rust]";
+        let entry = Entry::new(2, "Rust");
+
+        assert_eq!(format!("{}", entry), correct);
+    }
+
+    #[test]
+    fn value_does_not_affect_eq() {
+        let entry1 = Entry::new(3, "Rust");
+        let entry2 = Entry::new(3, "Haskell");
+
+        assert_eq!(entry1, entry2);
+    }
+
+    #[test]
+    fn entry_eq_is_same_as_timestamp_eq() {
+        for i in 0..100 {
+            for j in 0..100 {
+                assert_eq!(Entry::new(i, "") == Entry::new(j, ""), i == j);
+            }
+        }
+    }
+
+    #[test]
+    fn entry_leq_is_same_as_timestamp_leq() {
+        for i in 0..100 {
+            for j in 0..100 {
+                assert_eq!(Entry::new(i, "") <= Entry::new(j, ""), i <= j);
+            }
+        }
+    }
+
+    #[test]
+    fn entry_le_is_same_as_timestamp_le() {
+        for i in 0..100 {
+            for j in 0..100 {
+                assert_eq!(Entry::new(i, "") < Entry::new(j, ""), i < j);
+            }
+        }
+    }
+
+
+}
