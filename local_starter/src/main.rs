8b383735080dcfe78397417d06b1bb5f8fe220cd
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-07-06 15:36:00 +0200

    Add option to optimize the application code in local starter.

--- local_starter/src/main.rs
+++ local_starter/src/main.rs
@@ -14,12 +14,18 @@ fn main() {
     let number_of_nodes = number_of_nodes(&matches);
     let _number_of_writers = number_of_writers(&matches);
     let _number_of_readers = number_of_readers(&matches);
+    let release_mode = release_mode(&matches);
 
     create_hosts_file(number_of_nodes);
 
+    let mut release_string = "";
+    if release_mode {
+        release_string = "--release";
+    }
+
     let mut build_process = Command::new("/bin/bash")
         .arg("-c")
-        .arg(format!("cargo build --manifest-path ../application/Cargo.toml"))
+        .arg(format!("cargo build {} --manifest-path ../application/Cargo.toml", release_string))
         .spawn()
         .expect("failed to execute process");
 
@@ -30,7 +36,7 @@ fn main() {
         let color = color_from_node_id(node_id);
         let child_process = Command::new("/bin/bash")
                 .arg("-c")
-                .arg(format!("cargo run --manifest-path ../application/Cargo.toml -- {} hosts.txt {:?}", node_id, color))
+                .arg(format!("cargo run {} --manifest-path ../application/Cargo.toml -- {} hosts.txt {:?}", release_string, node_id, color))
                 .spawn()
                 .expect("failed to execute process");
 
@@ -50,17 +56,26 @@ fn get_matches() -> ArgMatches<'static> {
 
         .arg(Arg::with_name("number-of-nodes")
             .required(true)
+            .takes_value(true)
             .help("The number of local nodes to run."))
 
         .arg(Arg::with_name("number-of-writers")
             .short("w")
             .long("number-of-writers")
+            .takes_value(true)
             .help("The number of nodes that should write."))
 
         .arg(Arg::with_name("number-of-readers")
             .short("r")
             .long("number-of-readers")
+            .takes_value(true)
             .help("The number of nodes that should read."))
+
+        .arg(Arg::with_name("optimize")
+            .short("o")
+            .long("optimize")
+            .takes_value(false)
+            .help("With this option, cargo will build/run in release mode. This uses optimizations and yields higher performance."))
 
         .get_matches()
 }
@@ -115,4 +130,8 @@ fn hosts_file_string(number_of_nodes: i32) -> String {
 fn color_from_node_id(node_id: i32) -> Color {
     let colors = vec![Black, Red, Green, Yellow, Blue, Magenta, Cyan];
     colors[(node_id as usize) % 7]
+}
+
+fn release_mode(matches: &ArgMatches<'static>) -> bool {
+    matches.is_present("optimize")
 }
