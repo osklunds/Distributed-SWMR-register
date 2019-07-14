aafd76ec27d432d088287027fbd98b1c601ccb60
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-07-14 14:12:26 +0200

    Factor out arguments in local_starter.

---
+++ local_starter/src/arguments.rs
@@ -0,0 +1,137 @@
+
+use clap::{Arg, App, ArgMatches};
+
+lazy_static! {
+    pub static ref ARGUMENTS: Arguments = Arguments::new();
+}
+
+
+pub struct Arguments {
+    pub number_of_nodes: i32,
+    pub number_of_writers: i32,
+    pub number_of_readers: i32,
+    pub release_mode_string: String,
+    pub print_client_operations_string: String,
+    pub run_length_string: String,
+    pub record_evaluation_info_string: String
+}
+
+impl Arguments {
+    fn new() -> Arguments {
+        let matches = get_matches();
+
+        let number_of_nodes = number_of_nodes_from_matches(&matches);
+        let number_of_writers = number_of_writers_from_matches(&matches);
+        let number_of_readers = number_of_readers_from_matches(&matches);
+        let release_mode_string = release_mode_string_from_matches(&matches);
+        let print_client_operations_string = print_client_operations_string_from_matches(&matches);
+        let run_length_string = run_length_string_from_matches(&matches);
+        let record_evaluation_info_string = record_evaluation_info_string_from_matches(&matches);
+
+        Arguments {
+            number_of_nodes: number_of_nodes,
+            number_of_writers: number_of_writers,
+            number_of_readers: number_of_readers,
+            release_mode_string: release_mode_string,
+            print_client_operations_string: print_client_operations_string,
+            run_length_string: run_length_string,
+            record_evaluation_info_string: record_evaluation_info_string
+        }
+    }
+}
+
+
+fn get_matches() -> ArgMatches<'static> {
+    App::new("Distributed SWMR registers: Local starter")
+        .version("0.1")
+        .author("Oskar Lundström")
+        .about("Todo")
+
+        .arg(Arg::with_name("number-of-nodes")
+            .required(true)
+            .takes_value(true)
+            .short("n")
+            .long("number-of-nodes")
+            .help("The number of local nodes to run."))
+
+        .arg(Arg::with_name("number-of-writers")
+            .required(false)
+            .takes_value(true)
+            .default_value("0")
+            .short("w")
+            .long("number-of-writers")
+            .help("The number of nodes that should write."))
+
+        .arg(Arg::with_name("number-of-readers")
+            .required(false)
+            .takes_value(true)
+            .default_value("0")
+            .short("r")
+            .long("number-of-readers")
+            .help("The number of nodes that should read."))
+
+        .arg(Arg::with_name("optimize")
+            .takes_value(false)
+            .short("o")
+            .long("optimize")
+            .help("With this option, cargo will build/run in release mode. This uses optimizations and yields higher performance."))
+
+        .arg(Arg::with_name("print-client-operations")
+            .short("p")
+            .long("print-client-operations")
+            .takes_value(false)
+            .help("Print when a read/write operation starts/ends. If not included, the performance might be slightly higher."))
+
+        .arg(Arg::with_name("run-length")
+            .required(false)
+            .takes_value(true)
+            .default_value("0")
+            .short("l")
+            .long("run-length")
+            .help("The number of seconds the program should run for. If 0 is given, the program will until aborted with Ctrl-C."))
+
+        .arg(Arg::with_name("record-evaluation-info")
+            .short("e")
+            .long("record-evaluation-info")
+            .takes_value(false)
+            .help("Record information used for the evaluation, such as latency and number of messages sent. If not included, the performance might be slightly higher."))
+
+        .get_matches()
+}
+
+fn number_of_nodes_from_matches(matches: &ArgMatches<'static>) -> i32 {
+    matches.value_of("number-of-nodes").unwrap().parse().unwrap()
+}
+
+fn number_of_writers_from_matches(matches: &ArgMatches<'static>) -> i32 {
+    matches.value_of("number-of-writers").unwrap().parse().unwrap()
+}
+
+fn number_of_readers_from_matches(matches: &ArgMatches<'static>) -> i32 {
+    matches.value_of("number-of-readers").unwrap().parse().unwrap()
+}
+
+fn release_mode_string_from_matches(matches: &ArgMatches<'static>) -> String {
+    match matches.is_present("optimize") {
+        true  => "--release".to_string(),
+        false => "".to_string()
+    }
+}
+
+fn print_client_operations_string_from_matches(matches: &ArgMatches<'static>) -> String {
+    match matches.is_present("print-client-operations") {
+        true  => "--print-client-operations".to_string(),
+        false => "".to_string()
+    }
+}
+
+fn run_length_string_from_matches(matches: &ArgMatches<'static>) -> String {
+    matches.value_of("run-length").unwrap().to_string()
+}
+
+fn record_evaluation_info_string_from_matches(matches: &ArgMatches<'static>) -> String {
+    match matches.is_present("record-evaluation-info") {
+        true  => "--record-evaluation-info".to_string(),
+        false => "".to_string()
+    }
+}
--- local_starter/src/main.rs
+++ local_starter/src/main.rs
@@ -1,145 +1,35 @@
 
-use std::process::Command;
+#[macro_use]
+extern crate lazy_static;
+
+mod arguments;
+
+use std::process::{Command, Child};
 use std::fs;
 use std::path::Path;
 use std::vec::Vec;
 
-use clap::{Arg, App, ArgMatches};
 use colored::Color;
 use colored::Color::*;
 
+use crate::arguments::ARGUMENTS;
+
+
+type NodeId = i32;
+
 
 fn main() {
-    let matches = get_matches();
-    let number_of_nodes = number_of_nodes(&matches);
-    let number_of_writers = number_of_writers(&matches);
-    let number_of_readers = number_of_readers(&matches);
-    let release_mode_string = release_mode_string(&matches);
-    let print_client_operations_string = print_client_operations_string(&matches);
-    let run_length = matches.value_of("run-length").unwrap();
-    let record_evaluation_info_string = record_evaluation_info_string(&matches);
-
-    create_hosts_file(number_of_nodes);
-
-    let mut build_process = Command::new("/bin/bash")
-        .arg("-c")
-        .arg(format!("cargo build {} --manifest-path ../application/Cargo.toml", release_mode_string))
-        .spawn()
-        .expect("failed to execute process");
-
-    build_process.wait().unwrap();
-
-    let mut child_processes = Vec::new();
-    for node_id in 1..number_of_nodes+1 {
-        let color = color_from_node_id(node_id);
-        let write_string = match node_id <= number_of_writers {
-            true  => "--write",
-            false => ""
-        };
-        let read_string = match node_id <= number_of_readers {
-            true => "--read",
-            false => ""
-        };
-
-        let command_string = format!("cargo run {} --manifest-path ../application/Cargo.toml -- {} hosts.txt {} {:?} {} {} {} {}", release_mode_string, node_id, run_length, color, print_client_operations_string, record_evaluation_info_string, write_string, read_string);
-
-        println!("{}", command_string);
-
-        let child_process = Command::new("/bin/bash")
-                .arg("-c")
-                .arg(command_string)
-                .spawn()
-                .expect("failed to execute process");
-
-        child_processes.push(child_process);
-    }
- 
-    for child_process in child_processes.iter_mut() {
-        child_process.wait().unwrap();
-    }
+    create_hosts_file();
+    build_application();
+    run_application();
 }
 
-fn get_matches() -> ArgMatches<'static> {
-    App::new("Distributed SWMR registers: Local starter")
-        .version("0.1")
-        .author("Oskar Lundström")
-        .about("Todo")
-
-        .arg(Arg::with_name("number-of-nodes")
-            .required(true)
-            .takes_value(true)
-            .short("n")
-            .long("number-of-nodes")
-            .help("The number of local nodes to run."))
-
-        .arg(Arg::with_name("number-of-writers")
-            .required(true)
-            .takes_value(true)
-            .short("w")
-            .long("number-of-writers")
-            .help("The number of nodes that should write."))
-
-        .arg(Arg::with_name("number-of-readers")
-            .required(true)
-            .takes_value(true)
-            .short("r")
-            .long("number-of-readers")
-            .help("The number of nodes that should read."))
-
-        .arg(Arg::with_name("optimize")
-            .takes_value(false)
-            .short("o")
-            .long("optimize")
-            .help("With this option, cargo will build/run in release mode. This uses optimizations and yields higher performance."))
-
-        .arg(Arg::with_name("print-client-operations")
-            .short("p")
-            .long("print-client-operations")
-            .takes_value(false)
-            .help("Print when a read/write operation starts/ends. If not included, the performance might be slightly higher."))
-
-        .arg(Arg::with_name("run-length")
-            .takes_value(true)
-            .required(true)
-            .short("l")
-            .long("run-length")
-            .help("The number of seconds the program should run for. If 0 is given, the program will until aborted with Ctrl-C."))
-
-        .arg(Arg::with_name("record-evaluation-info")
-            .short("e")
-            .long("record-evaluation-info")
-            .takes_value(false)
-            .help("Record information used for the evaluation, such as latency and number of messages sent. If not included, the performance might be slightly higher."))
-
-        .get_matches()
-}
-
-fn number_of_nodes(matches: &ArgMatches<'static>) -> i32 {
-    matches.value_of("number-of-nodes").unwrap().parse().unwrap()
-}
-
-fn number_of_writers(matches: &ArgMatches<'static>) -> i32 {
-    if let Some(number_of_writers) = matches.value_of("number-of-writers") {
-        number_of_writers.parse().unwrap()
-    } else {
-        0
-    }
-}
-
-fn number_of_readers(matches: &ArgMatches<'static>) -> i32 {
-    if let Some(number_of_readers) = matches.value_of("number-of-readers") {
-        number_of_readers.parse().unwrap()
-    } else {
-        0
-    }
-}
-
-fn create_hosts_file(number_of_nodes: i32) {
-    let correct_string = hosts_file_string(number_of_nodes);
+fn create_hosts_file() {
+    let hosts_file_string = hosts_file_string();
     let file_path = Path::new("hosts.txt");
     if file_path.exists() {
         if let Ok(existing_string) = fs::read_to_string(file_path) {
-            if existing_string == correct_string {
+            if existing_string == hosts_file_string {
                 return;
             }
         }
@@ -147,49 +37,69 @@ fn create_hosts_file(number_of_nodes: i32) {
         fs::remove_file(file_path).expect("Could not remove existing hosts.txt file");
     }
 
-    fs::write(file_path, correct_string).expect("Could not write new hosts.txt file.");
+    fs::write(file_path, hosts_file_string).expect("Could not write new hosts.txt file.");
 }
 
-fn hosts_file_string(number_of_nodes: i32) -> String {
+fn hosts_file_string() -> String {
     let mut string = String::new();
     let port_offset = 62000;
 
-    for node_id in 1..number_of_nodes+1 {
+    for node_id in 1..ARGUMENTS.number_of_nodes+1 {
         string.push_str(&format!("{},127.0.0.1:{}\n", node_id, node_id + port_offset));
     }
 
     string
 }
 
+fn build_application() {
+    let command = format!("cargo build {} --manifest-path ../application/Cargo.toml", ARGUMENTS.release_mode_string);
+    let mut build_process = execute_local_command(&command);
+    build_process.wait().unwrap();
+}
+
+fn execute_local_command(command: &str) -> Child {
+    Command::new("/bin/bash")
+        .arg("-c")
+        .arg(command)
+        .spawn()
+        .expect(&format!("Failed to execute the command: {}", command))
+}
+
+fn run_application() {
+    let mut run_processes = Vec::new();
+    for node_id in 1..ARGUMENTS.number_of_nodes+1 {
+        let run_process = run_single_application_instance(node_id);
+        run_processes.push(run_process);
+    }
+ 
+    for run_process in run_processes.iter_mut() {
+        run_process.wait().unwrap();
+    }
+}
+
+fn run_single_application_instance(node_id: NodeId) -> Child {
+    let color = color_from_node_id(node_id);
+    let write_string = match node_id <= ARGUMENTS.number_of_writers {
+        true  => "--write",
+        false => ""
+    };
+    let read_string = match node_id <= ARGUMENTS.number_of_readers {
+        true => "--read",
+        false => ""
+    };
+
+    let command = format!("cargo run {} --manifest-path ../application/Cargo.toml -- {} hosts.txt {} {:?} {} {} {} {}", 
+        ARGUMENTS.release_mode_string, node_id, 
+        ARGUMENTS.run_length_string, color, 
+        ARGUMENTS.print_client_operations_string, 
+        ARGUMENTS.record_evaluation_info_string, 
+        write_string, 
+        read_string);
+
+    execute_local_command(&command)
+}
+
 fn color_from_node_id(node_id: i32) -> Color {
     let colors = vec![Black, Red, Green, Yellow, Blue, Magenta, Cyan];
     colors[(node_id as usize) % 7]
-}
-
-fn release_mode_string(matches: &ArgMatches<'static>) -> String {
-    if release_mode(matches) {
-        String::from("--release")
-    } else {
-        String::from("")
-    }
-}
-
-fn release_mode(matches: &ArgMatches<'static>) -> bool {
-    matches.is_present("optimize")
-}
-
-fn print_client_operations_string(matches: &ArgMatches<'static>) -> String {
-    if matches.is_present("print-client-operations") {
-        "--print-client-operations".to_string()
-    } else {
-        "".to_string()
-    }
-}
-
-fn record_evaluation_info_string(matches: &ArgMatches<'static>) -> String {
-    if matches.is_present("record-evaluation-info") {
-        "--record-evaluation-info".to_string()
-    } else {
-        "".to_string()
-    }
 }
