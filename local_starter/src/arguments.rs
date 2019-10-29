09323e9f4615c4eda64209e49fd536596f7237ed
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2019-10-29 17:04:53 +0100

    fmt local_starter.

--- local_starter/src/arguments.rs
+++ local_starter/src/arguments.rs
@@ -1,14 +1,11 @@
-
-use clap::{Arg, App, ArgMatches, AppSettings};
+use clap::{App, AppSettings, Arg, ArgMatches};
 
 use commons::arguments;
 use commons::types::Int;
 
-
 lazy_static! {
     pub static ref ARGUMENTS: Arguments = Arguments::new();
 }
-
 
 pub struct Arguments {
     pub number_of_nodes: Int,
@@ -17,32 +14,34 @@ pub struct Arguments {
     pub release_mode_string: String,
     pub print_client_operations_string: String,
     pub run_length_string: String,
-    pub record_evaluation_info_string: String
+    pub record_evaluation_info_string: String,
 }
 
 impl Arguments {
     fn new() -> Arguments {
         let matches = get_matches();
-        
+
         Arguments {
             number_of_nodes: number_of_nodes_from_matches(&matches),
             number_of_writers: arguments::number_of_writers_from_matches(&matches),
             number_of_readers: arguments::number_of_readers_from_matches(&matches),
             release_mode_string: arguments::release_mode_string_from_matches(&matches),
-            print_client_operations_string: arguments::print_client_operations_string_from_matches(&matches),
+            print_client_operations_string: arguments::print_client_operations_string_from_matches(
+                &matches,
+            ),
             run_length_string: arguments::run_length_string_from_matches(&matches),
-            record_evaluation_info_string: arguments::record_evaluation_info_string_from_matches(&matches)
+            record_evaluation_info_string: arguments::record_evaluation_info_string_from_matches(
+                &matches,
+            ),
         }
     }
 }
-
 
 fn get_matches() -> ArgMatches<'static> {
     App::new("Distributed-SWMR-registers: Local starter")
         .setting(AppSettings::DisableVersion)
         .setting(AppSettings::VersionlessSubcommands)
         .about("A helper utility that starts multiple nodes on your local computer.")
-
         .arg(number_of_nodes_argument())
         .arg(arguments::number_of_writers())
         .arg(arguments::number_of_readers())
@@ -50,7 +49,6 @@ fn get_matches() -> ArgMatches<'static> {
         .arg(arguments::print_client_operations())
         .arg(arguments::run_length())
         .arg(arguments::record_evaluation_info())
-
         .get_matches()
 }
 
@@ -64,5 +62,9 @@ pub fn number_of_nodes_argument() -> Arg<'static, 'static> {
 }
 
 fn number_of_nodes_from_matches(matches: &ArgMatches<'static>) -> Int {
-    matches.value_of("number-of-nodes").expect("Number of nodes arg not existing.").parse().expect("Could not parse number of nodes.")
+    matches
+        .value_of("number-of-nodes")
+        .expect("Number of nodes arg not existing.")
+        .parse()
+        .expect("Could not parse number of nodes.")
 }
--- local_starter/src/main.rs
+++ local_starter/src/main.rs
@@ -1,20 +1,18 @@
-
 #[macro_use]
 extern crate lazy_static;
 
 mod arguments;
 
-use std::process::Child;
 use std::fs;
 use std::path::Path;
+use std::process::Child;
 use std::vec::Vec;
 
 use commons::execution;
-use commons::types::NodeId;
 use commons::misc;
+use commons::types::NodeId;
 
 use crate::arguments::ARGUMENTS;
-
 
 fn main() {
     create_hosts_file();
@@ -42,51 +40,61 @@ fn hosts_file_string() -> String {
     let mut string = String::new();
     let port_offset = 62000;
 
-    for node_id in 1..ARGUMENTS.number_of_nodes+1 {
-        string.push_str(&format!("{},127.0.0.1:{}\n", node_id, node_id + port_offset));
+    for node_id in 1..ARGUMENTS.number_of_nodes + 1 {
+        string.push_str(&format!(
+            "{},127.0.0.1:{}\n",
+            node_id,
+            node_id + port_offset
+        ));
     }
 
     string
 }
 
 fn build_application() {
-    let command = format!("cargo build {} --manifest-path ../application/Cargo.toml", 
-        ARGUMENTS.release_mode_string);
+    let command = format!(
+        "cargo build {} --manifest-path ../application/Cargo.toml",
+        ARGUMENTS.release_mode_string
+    );
     let mut build_process = execution::execute_local_command(&command);
-    build_process.wait().expect("Could not wait for the build process.");
+    build_process
+        .wait()
+        .expect("Could not wait for the build process.");
 }
 
 fn run_application() {
     let mut run_processes = Vec::new();
-    for node_id in 1..ARGUMENTS.number_of_nodes+1 {
+    for node_id in 1..ARGUMENTS.number_of_nodes + 1 {
         let run_process = run_single_application_instance(node_id);
         run_processes.push(run_process);
     }
- 
+
     for run_process in run_processes.iter_mut() {
-        run_process.wait().expect("Could not wait for the run process.");
+        run_process
+            .wait()
+            .expect("Could not wait for the run process.");
     }
 }
 
 fn run_single_application_instance(node_id: NodeId) -> Child {
     let color = misc::color_from_node_id(node_id);
     let write_string = match node_id <= ARGUMENTS.number_of_writers {
-        true  => "--write",
-        false => ""
+        true => "--write",
+        false => "",
     };
     let read_string = match node_id <= ARGUMENTS.number_of_readers {
         true => "--read",
-        false => ""
+        false => "",
     };
 
     let command = format!("cargo run {} --manifest-path ../application/Cargo.toml -- {} hosts.txt -c {:?} -l {} {} {} {} {}", 
-        ARGUMENTS.release_mode_string, 
-        node_id, 
+        ARGUMENTS.release_mode_string,
+        node_id,
         color,
-        ARGUMENTS.run_length_string, 
-        ARGUMENTS.print_client_operations_string, 
-        ARGUMENTS.record_evaluation_info_string, 
-        write_string, 
+        ARGUMENTS.run_length_string,
+        ARGUMENTS.print_client_operations_string,
+        ARGUMENTS.record_evaluation_info_string,
+        write_string,
         read_string);
 
     execution::execute_local_command(&command)
